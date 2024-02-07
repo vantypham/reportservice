3412d743bda8f9dad1007a59f819737391cb0bdc
EthanNguyen132 <huytoan132@gmail.com>
2024-02-07 14:47:48 -0600

    minor change

--- SWAReport/src/environments/environment.prod.ts
+++ SWAReport/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://cs590priusreportservice.azurewebsites.net/report'
+    baseUrl: 'https://cs590priusreportservice.azurewebsites.net/report',
+    // baseUrl: 'http://20.189.79.251:8585/report'
 };
